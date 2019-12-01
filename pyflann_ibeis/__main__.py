b169de41bde79c0bb9b74f97b8351ab35b10eabe
joncrall <erotemic@gmail.com>
2019-12-01 18:41:48 -0500

    wip

--- pyflann_ibeis/__main__.py
+++ pyflann_ibeis/__main__.py
@@ -1,9 +1,10 @@
 
 
-def main():
+def main():  # nocover
     import pyflann_ibeis
     print('Looks like the imports worked')
     print('pyflann_ibeis = {!r}'.format(pyflann_ibeis))
+    print('pyflann_ibeis.__file__ = {!r}'.format(pyflann_ibeis.__file__))
     print('pyflann_ibeis.__version__ = {!r}'.format(pyflann_ibeis.__version__))
     print('pyflann_ibeis.flann_ctypes.flannlib = {!r}'.format(pyflann_ibeis.flann_ctypes.flannlib))
     print('pyflann_ibeis.flann_ctypes.libpath = {!r}'.format(pyflann_ibeis.flann_ctypes.libpath))
