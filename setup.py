d78c0bb677db9a0e913ed050b30043440732f248
joncrall <erotemic@gmail.com>
2024-04-16 10:48:43 -0400

    wip

--- setup.py
+++ setup.py
@@ -15,149 +15,165 @@ Build Wheels:
 """
 from __future__ import absolute_import, division, print_function
 import sys
-from os.path import exists
 from collections import OrderedDict
+import re
+from os.path import exists, dirname, join
 
 
-def native_mb_python_tag(plat_impl=None, version_info=None):
-    """
-    Example:
-        >>> print(native_mb_python_tag())
-        >>> print(native_mb_python_tag('PyPy', (2, 7)))
-        >>> print(native_mb_python_tag('CPython', (3, 8)))
-    """
-    if plat_impl is None:
-        import platform
-        plat_impl = platform.python_implementation()
-
-    if version_info is None:
-        import sys
-        version_info = sys.version_info
-
-    major, minor = version_info[0:2]
-    ver = '{}{}'.format(major, minor)
-
-    if plat_impl == 'CPython':
-        # TODO: get if cp27m or cp27mu
-        impl = 'cp'
-        if ver == '27':
-            IS_27_BUILT_WITH_UNICODE = True  # how to determine this?
-            if IS_27_BUILT_WITH_UNICODE:
-                abi = 'mu'
-            else:
-                abi = 'm'
-        else:
-            if ver == '38':
-                # no abi in 38?
-                abi = ''
-            else:
-                abi = 'm'
-        mb_tag = '{impl}{ver}-{impl}{ver}{abi}'.format(**locals())
-    elif plat_impl == 'PyPy':
-        abi = ''
-        impl = 'pypy'
-        ver = '{}{}'.format(major, minor)
-        mb_tag = '{impl}-{ver}'.format(**locals())
-    else:
-        raise NotImplementedError(plat_impl)
-    return mb_tag
-
-
-def parse_version(fpath='pyflann_ibeis/__init__.py'):
+def parse_version(fpath):
     """
     Statically parse the version number from a python file
+    """
+    value = static_parse("__version__", fpath)
+    return value
 
 
+def static_parse(varname, fpath):
+    """
+    Statically parse the a constant variable from a python file
     """
     import ast
+
     if not exists(fpath):
-        raise ValueError('fpath={!r} does not exist'.format(fpath))
-    with open(fpath, 'r') as file_:
+        raise ValueError("fpath={!r} does not exist".format(fpath))
+    with open(fpath, "r") as file_:
         sourcecode = file_.read()
     pt = ast.parse(sourcecode)
-    class VersionVisitor(ast.NodeVisitor):
+
+    class StaticVisitor(ast.NodeVisitor):
         def visit_Assign(self, node):
             for target in node.targets:
-                if getattr(target, 'id', None) == '__version__':
-                    self.version = node.value.s
-    visitor = VersionVisitor()
+                if getattr(target, "id", None) == varname:
+                    self.static_value = node.value.s
+
+    visitor = StaticVisitor()
     visitor.visit(pt)
-    return visitor.version
+    try:
+        value = visitor.static_value
+    except AttributeError:
+        import warnings
+
+        value = "Unknown {}".format(varname)
+        warnings.warn(value)
+    return value
 
 
-def parse_long_description(fpath='README.rst'):
+def parse_description():
     """
-    Reads README text, but doesn't break if README does not exist.
-    """
-    if exists(fpath):
-        with open(fpath, 'r') as file:
-            return file.read()
-    return ''
-
-
-def parse_requirements(fname='requirements.txt'):
-    """
-    Parse the package dependencies listed in a requirements file but
-    strips specific versioning information.
+    Parse the description in the README file
 
     CommandLine:
-        python -c "import setup; print(setup.parse_requirements())"
+        pandoc --from=markdown --to=rst --output=README.rst README.md
+        python -c "import setup; print(setup.parse_description())"
     """
-    import re
+    readme_fpath = join(dirname(__file__), "README.rst")
+    # This breaks on pip install, so check that it exists.
+    if exists(readme_fpath):
+        with open(readme_fpath, "r") as f:
+            text = f.read()
+        return text
+    return ""
+
+
+def parse_requirements(fname="requirements.txt", versions=False):
+    """
+    Parse the package dependencies listed in a requirements file but strips
+    specific versioning information.
+
+    Args:
+        fname (str): path to requirements file
+        versions (bool | str, default=False):
+            If true include version specs.
+            If strict, then pin to the minimum version.
+
+    Returns:
+        List[str]: list of requirements items
+
+    CommandLine:
+        python -c "import setup, ubelt; print(ubelt.urepr(setup.parse_requirements()))"
+    """
     require_fpath = fname
 
-    def parse_line(line):
+    def parse_line(line, dpath=""):
         """
         Parse information from a line in a requirements text file
+
+        line = 'git+https://a.com/somedep@sometag#egg=SomeDep'
+        line = '-e git+https://a.com/somedep@sometag#egg=SomeDep'
         """
-        if line.startswith('-r '):
+        # Remove inline comments
+        comment_pos = line.find(" #")
+        if comment_pos > -1:
+            line = line[:comment_pos]
+
+        if line.startswith("-r "):
             # Allow specifying requirements in other files
-            target = line.split(' ')[1]
+            target = join(dpath, line.split(" ")[1])
             for info in parse_require_file(target):
                 yield info
-        elif line.startswith('-e '):
-            info = {}
-            info['package'] = line.split('#egg=')[1]
-            yield info
         else:
-            # Remove versioning from the package
-            pat = '(' + '|'.join(['>=', '==', '>']) + ')'
-            parts = re.split(pat, line, maxsplit=1)
-            parts = [p.strip() for p in parts]
-
-            info = {}
-            info['package'] = parts[0]
-            if len(parts) > 1:
-                op, rest = parts[1:]
-                if ';' in rest:
+            # See: https://www.python.org/dev/peps/pep-0508/
+            info = {"line": line}
+            if line.startswith("-e "):
+                info["package"] = line.split("#egg=")[1]
+            else:
+                if "--find-links" in line:
+                    # setuptools doesnt seem to handle find links
+                    line = line.split("--find-links")[0]
+                if ";" in line:
+                    pkgpart, platpart = line.split(";")
                     # Handle platform specific dependencies
-                    # http://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-platform-specific-dependencies
-                    version, platform_deps = map(str.strip, rest.split(';'))
-                    info['platform_deps'] = platform_deps
+                    # setuptools.readthedocs.io/en/latest/setuptools.html
+                    # #declaring-platform-specific-dependencies
+                    plat_deps = platpart.strip()
+                    info["platform_deps"] = plat_deps
                 else:
+                    pkgpart = line
+                    platpart = None
+
+                # Remove versioning from the package
+                pat = "(" + "|".join([">=", "==", ">"]) + ")"
+                parts = re.split(pat, pkgpart, maxsplit=1)
+                parts = [p.strip() for p in parts]
+
+                info["package"] = parts[0]
+                if len(parts) > 1:
+                    op, rest = parts[1:]
                     version = rest  # NOQA
-                info['version'] = (op, version)
+                    info["version"] = (op, version)
             yield info
 
     def parse_require_file(fpath):
-        with open(fpath, 'r') as f:
+        dpath = dirname(fpath)
+        with open(fpath, "r") as f:
             for line in f.readlines():
                 line = line.strip()
-                if line and not line.startswith('#'):
-                    for info in parse_line(line):
+                if line and not line.startswith("#"):
+                    for info in parse_line(line, dpath=dpath):
                         yield info
 
-    # This breaks on pip install, so check that it exists.
-    packages = []
-    if exists(require_fpath):
-        for info in parse_require_file(require_fpath):
-            package = info['package']
-            if not sys.version.startswith('3.4'):
-                # apparently package_deps are broken in 3.4
-                platform_deps = info.get('platform_deps')
-                if platform_deps is not None:
-                    package += ';' + platform_deps
-            packages.append(package)
+    def gen_packages_items():
+        if exists(require_fpath):
+            for info in parse_require_file(require_fpath):
+                parts = [info["package"]]
+                if versions and "version" in info:
+                    if versions == "strict":
+                        # In strict mode, we pin to the minimum version
+                        if info["version"]:
+                            # Only replace the first >= instance
+                            verstr = "".join(info["version"]).replace(">=", "==", 1)
+                            parts.append(verstr)
+                    else:
+                        parts.extend(info["version"])
+                if not sys.version.startswith("3.4"):
+                    # apparently package_deps are broken in 3.4
+                    plat_deps = info.get("platform_deps")
+                    if plat_deps is not None:
+                        parts.append(";" + plat_deps)
+                item = "".join(parts)
+                yield item
+
+    packages = list(gen_packages_items())
     return packages
 
 
@@ -178,7 +194,6 @@ except Exception:
 NAME = 'pyflann_ibeis'
 
 
-MB_PYTHON_TAG = native_mb_python_tag()  # NOQA
 VERSION = version = parse_version('pyflann_ibeis/__init__.py')  # must be global for git tags
 
 AUTHORS = ['Jon Crall', 'Marius Muja']
@@ -218,7 +233,7 @@ KWARGS = OrderedDict(
     author=', '.join(AUTHORS),
     author_email=AUTHOR_EMAIL,
     description=DESCRIPTION,
-    long_description=parse_long_description('README.rst'),
+    long_description=parse_description(),
     long_description_content_type='text/x-rst',
     url=URL,
     license=LICENSE,
