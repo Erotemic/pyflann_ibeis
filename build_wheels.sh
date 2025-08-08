21a32c39ee8b507e946ec2849a35df815630f83f
joncrall <erotemic@gmail.com>
2025-08-08 17:54:23 -0400

    wip

--- build_wheels.sh
+++ build_wheels.sh
@@ -22,3 +22,6 @@ fi
 #pip wheel -w wheelhouse .
 # python -m build --wheel -o wheelhouse  #  pyflann_ibeis: +COMMENT_IF(binpy)
 cibuildwheel --config-file pyproject.toml --platform linux --archs x86_64  #  pyflann_ibeis: +UNCOMMENT_IF(binpy)
+
+export CIBW_CONFIG_SETTINGS: --build-option=--py-limited-api=cp38
+cibuildwheel --config-file pyproject.toml --platform linux --archs x86_64  #  pyflann_ibeis: +UNCOMMENT_IF(binpy)
